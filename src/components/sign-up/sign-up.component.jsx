6090d1e20588f49642824dff1855d15fc1aa9033
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-02-01 13:10:03 -0500

    updated sign up component to be class component + imported custom button + InputForm

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,6 +1,19 @@
 import react from "react";
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 import "./sign-up.styles.scss";
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 
-const SignUp = () => <div>Sign up</div>;
+class SignUp extends RemotePlayback.component {
+  constructor() {
+    super();
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+}
 
 export default SignUp;
