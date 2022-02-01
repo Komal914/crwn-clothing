b4c814e958a1d6245063c90a71bbbb31109a8ad4
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-02-01 13:35:02 -0500

    implemented handleSumbit fucntion to create user and pass, update state in app

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -2,7 +2,11 @@ import react from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import "./sign-up.styles.scss";
-import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
+import {
+  auth,
+  createuserProfileDocument,
+  createUserProfileDocument,
+} from "../../firebase/firebase.utils";
 
 class SignUp extends RemotePlayback.component {
   constructor() {
@@ -14,7 +18,39 @@ class SignUp extends RemotePlayback.component {
       confirmPassword: "",
     };
   }
+  //recieves the event
+  handleSubmit = async (event) => {
+    event.preventDefault(); //avoids default action
+    const { displayName, password, confirmPassword, email } = this.state;
 
+    //case 1: password and confirmpass do not match
+    //returns if they do not match
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    //try catch block to create if pass matches confirmpass
+    try {
+      //creates the user and pass with userauth
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      //creates userref
+      await createuserProfileDocument(user, { displayName });
+      //wait for creating user and pass, then change the state back to empty for the app so users can make more accounts
+      //clears the form
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    } catch (error) {
+      console.log("Error creating sign up: ", error);
+    }
+  };
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
