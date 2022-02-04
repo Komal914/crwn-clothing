540352ff3d55bb2030250958782408645bd8c1d3
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-02-04 13:57:53 -0500

    fixed typo

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import userReducer from "./user/udeser.reducer";
+import userReducer from "./user/user.reducer";
 
 export default combineReducers({
   user: userReducer,
