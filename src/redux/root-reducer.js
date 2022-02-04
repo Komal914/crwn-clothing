0ef07ded3ad62ab402b25eefd450b71af0137153
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-02-04 13:53:58 -0500

    initialized user as userReducer in combineReducers + imported logger, createstoer, applyMiddleware for store.js to create our store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/udeser.reducer";
 
-export default combineReducers({});
+export default combineReducers({
+  user: userReducer,
+});
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
+
+//setting up middleware
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
