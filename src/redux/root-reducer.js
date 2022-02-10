75117d3b0f8a2b4be966fe2ccd0995ab8dbdb564
Komal Kaur <44416323+Komal914@users.noreply.github.com>
2022-02-10 17:12:01 -0500

    added cartReducer into our root reducer file

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 //redux lets us combine multiple reducers into one
 //that can be passed into createStrore
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
